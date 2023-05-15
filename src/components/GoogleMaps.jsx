f36919293916a46758042b87d431bacca3dd4581
Tons <38555090+tonsisouvanh@users.noreply.github.com>
2023-05-15 21:18:47 +0700

    changed api key field name in yml file

--- src/components/GoogleMaps.jsx
+++ src/components/GoogleMaps.jsx
@@ -12,37 +12,37 @@ const center = {
 };
 
 const GoogleMaps = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,
-  });
+  // const { isLoaded } = useJsApiLoader({
+  //   id: "google-map-script",
+  //   googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API,
+  // });
 
-  const [map, setMap] = React.useState(null);
+  // const [map, setMap] = React.useState(null);
 
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+  // const onLoad = React.useCallback(function callback(map) {
+  //   // This is just an example of getting and using the map instance!!! don't just blindly copy!
+  //   const bounds = new window.google.maps.LatLngBounds(center);
+  //   map.fitBounds(bounds);
 
-    setMap(map);
-  }, []);
+  //   setMap(map);
+  // }, []);
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
-  }, []);
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      <Marker position={center} />
-    </GoogleMap>
-  ) : (
-    <></>
-  );
+  // const onUnmount = React.useCallback(function callback(map) {
+  //   setMap(null);
+  // }, []);
+  // return isLoaded ? (
+  //   <GoogleMap
+  //     mapContainerStyle={containerStyle}
+  //     center={center}
+  //     zoom={10}
+  //     onLoad={onLoad}
+  //     onUnmount={onUnmount}
+  //   >
+  //     <Marker position={center} />
+  //   </GoogleMap>
+  // ) : (
+  //   <></>
+  // );
 };
 
 export default GoogleMaps;
